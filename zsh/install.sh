ce028e432a9fd71b03ec46a606ebf0724068b4c3
JD <64992493+hiringManager@users.noreply.github.com>
2022-04-07 08:40:13 -0500

    Update install.sh

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,6 +1,14 @@
-# Antigen, autocomplete, .zshrc
+# Installing antigen, zsh-autocomplete, and moving configuration.
+
+# Creating junk directory
 mkdir ~/.zsh
+
+ # Installing Antigen
 curl -L git.io/antigen > ~/.zsh/antigen.zsh
 sleep 2
-git clone https://github.com/marlonrichert/zsh-autocomplete ~/.antigen/bundles/zsh-autocomplete
+
+# Manually cloning this since it always breaks
+git clone https://github.com/marlonrichert/zsh-autocomplete ~/.antigen/bundles/zsh-autocomplete 
+
+# Moving configs
 mv .zshrc .aliasrc ~/

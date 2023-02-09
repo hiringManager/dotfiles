57e587a4a419f71fe6f9ab1bde514edb972fbf57
JD <64992493+hiringManager@users.noreply.github.com>
2023-02-09 09:57:23 -0600

    Update fedora-convenience-script.sh

--- .scripts/fedora-convenience-script.sh
+++ .scripts/fedora-convenience-script.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
+# Repos 
+
+# Flathub 
+# https://flatpak.org/setup/Fedora
+
 # Adding flathub to repos. Allows 3rd party stuff. Also consider rpm fusion if you don't have it already
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
+# Rpm Fusion
+# https://docs.fedoraproject.org/en-US/quick-docs/setup_rpmfusion/
+
+# sudo dnf install \
+#   https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+
+# sudo dnf install \
+#   https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
 # Docker
+# https://docs.docker.com/engine/install/fedora/#prerequisites
 
 # Removing older versions of docker if they exist
-# https://docs.docker.com/engine/install/fedora/#prerequisites
 sudo dnf remove docker \
                   docker-client \
                   docker-client-latest \
@@ -29,16 +43,17 @@ sudo dnf config-manager \
 # Docker installation
 sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# May need to enable
 sudo systemctl start docker
+
+# May need to enable
 # sudo systemctl enable docker
 
-# Hopefully fucking users aren't a problem here. Dumb Docker
+# Hopefully fucking users aren't a problem here. Not sure in fedora
 
 # Vscode 
+# https://code.visualstudio.com/docs/setup/linux
 
 # Vscode repo
-# https://code.visualstudio.com/docs/setup/linux
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sleep 1
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
@@ -49,6 +64,7 @@ dnf check-update
 sudo dnf install code
 
 # Node 
+# https://developer.fedoraproject.org/tech/languages/nodejs/nodejs.html
 
 # No unnecessary stuff with npm/npx etc in fedora
 # sudo dnf install nodejs
